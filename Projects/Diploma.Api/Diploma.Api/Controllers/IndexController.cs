8072796a2654ee01c6f3a4af6309f5e13b9e4a4b
Vitali Kaliabianau <kolebynov1996@yandex.ru>
2019-09-08 14:24:08 +0300

    Inital

---
+++ Projects/Diploma.Api/Diploma.Api/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diploma.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class IndexController : ControllerBase
+	{
+		[HttpPost]
+		public void Post([FromForm] string value)
+		{
+		}
+	}
+}
