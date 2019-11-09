272ec50a6d5bd0acc81827abcee4c37d3eb34389
Vitali Kaliabianau <kolebynov1996@yandex.ru>
2019-11-09 21:46:44 +0300

    k

--- Projects/src/Services/IndexingService/Diploma.IndexingService.Api/ClientApp/src/scripts/components/tab2/tab2.component.tsx
+++ Projects/src/Services/IndexingService/Diploma.IndexingService.Api/ClientApp/src/scripts/components/tab2/tab2.component.tsx
@@ -1,7 +1,17 @@
-import React, { FunctionComponent, memo } from 'react';
+import React, { FunctionComponent, memo, useState, useEffect } from 'react';
 
-const Tab2: FunctionComponent = memo(() => (
-  <div>Tab2</div>
-));
+const Tab2: FunctionComponent = memo(() => {
+  const [state, setState] = useState({
+    sosat: 1
+  });
+
+  useEffect(() => {
+    setTimeout(() => setState({
+      sosat: 2
+    }), 1000);
+  });
+
+  return (<div>Tab2: {state.sosat}</div>);
+});
 
 export default Tab2;
